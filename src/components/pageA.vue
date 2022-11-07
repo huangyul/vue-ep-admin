2487e4c8191c471e8e7befc038d191b199f48c78
huangyul <1103221645@qq.com>
2022-11-07 14:27:03 +0800

    feat: add sass

--- src/components/pageA.vue
+++ src/components/pageA.vue
@@ -1,3 +1,17 @@
-<template>page a</template>
+<template>
+  <div class="a">
+    a
+    <div class="b">b</div>
+  </div>
+</template>
 
 <script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.a {
+  background: red;
+  .b {
+    background: blue;
+  }
+}
+</style>
