ddfef003df2dfddaf79c84ce095806fe7e55b79d
huangyul <1103221645@qq.com>
2022-11-07 11:30:44 +0800

    feat: add vue test basis config

---
+++ src/__tests__/helloworld/index.spec.js
@@ -0,0 +1,17 @@
+// src/__tests__/helloworld/index.spec.js
+import { mount } from '@vue/test-utils'
+import HelloWorld from '@/HelloWorld.vue'
+
+test('displays message', async () => {
+  const wrapper = await mount(HelloWorld, {
+    props: {
+      msg: 'test msg'
+    }
+  })
+
+  // Assert the rendered text of the component
+  expect(wrapper.find('h1').text()).toBe('test msg')
+  expect(wrapper.find('button').text()).toBe('count is 0')
+  await wrapper.find('button').trigger('click')
+  expect(wrapper.find('button').text()).toBe('count is 1')
+})
