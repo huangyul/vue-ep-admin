364e14ab62f369d9fccb038174cd8f729de578ac
huangyul <1103221645@qq.com>
2022-11-09 15:24:35 +0800

    fix: delete vueconfig file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,11 @@
 // 默认配置
-import { vueConfig, netConfig, settingConfig, themeConfig } from "./default";
+import { netConfig, settingConfig, themeConfig } from "./default";
 // 自定义配置
 import customConfig from "./customConfig";
 
 // 导出配置（自定义配置优先级高）
 export default Object.assign(
   {},
-  vueConfig,
   netConfig,
   settingConfig,
   themeConfig,
